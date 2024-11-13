6b37bb4aa661dddf664f02dc0d2508dd355b8bf4
Aydan Ferrao <aydanf@nycstudents.net>
2024-11-13 11:01:19 -0500

    Started proble

---
+++ Problem_1/Driver.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Driver {
+  public static void main(String[] args) {
+    try {
+      File file = new File("file.txt");
+      Scanner input = new Scanner(file);
+      ArrayList<String> numList = new ArrayList<String>();
+      while (input.hasNextLine()) {
+
+      }
+    }
+  }
+}
